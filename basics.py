080c5823a727b27a2407381ca1f3af51365f9db9
Samuel <samuelnz36@gmail.com>
2025-01-22 20:11:45 +0530

    Files added

---
+++ basics.py
@@ -0,0 +1,8 @@
+first_name = "John"
+last_name = "Smith"
+age = 20
+is_new = True
+
+print (first_name+" "+last_name)
+print (age)
+print ("New =",is_new)
---
+++ conversion.py
@@ -0,0 +1,13 @@
+# birth_year = input("Enter your birth year: ")
+# age = 2024 - int(birth_year)
+# print ("Age = ",age)
+
+# a = input("First: ")
+# b = input("Second: ")
+# c = float(a) + float(b)
+# print("Sum: "+str(c))
+
+a = float(input("First: "))
+b = float(input("Second: "))
+c = a + b
+print("Sum: " + str(c))
